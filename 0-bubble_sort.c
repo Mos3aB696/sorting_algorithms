8e55ae95a05e301b5789d2d045ca82e0195d3043
Mos3aB696 <mos3ab696@gmail.com>
2023-11-20 01:25:45 +0200

    Ta_0_sk

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * swap - swap function
+ * @xp: The Position Of Previous Element
+ * @yp: The Position Of Current Element
+ *
+ * Return: void
+ */
+
+void swap(int *xp, int *yp)
+{
+	int tmp = *xp;
+	*xp = *yp;
+	*yp = tmp;
+}
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+		if (swapped == false)
+			break;
+	}
+}
