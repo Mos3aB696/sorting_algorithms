65e99f773721ebdf33f34c39f4b1d7ec55148bef
Amirametwally <amira.metwally56@gmail.com>
2023-11-21 00:15:59 +0200

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,51 +7,58 @@
  *
  * Return: nothing
  */
-void swap(int *a, int *b)
+void swap(int *xp, int *yp)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
 
 /**
- * _partition - function that used tp partition the
+ * _partition - function that used tp partition the 
  * array around povit
  * @arr: input array
  * @low: index for the first element
  * @high: index for the last element
  *
- * Return: index
+ * Return: index 
  */
 int _partition(int arr[], int low, int high)
 {
-	int pivot = arr[high];
-	int i = (low - 1);
-	int j;
+    int pivot = arr[high];
+    int i = (low - 1);
+    int j;
 
-	for (j = low; j <= high - 1; j++)
-	{
-		if (arr[j] < pivot)
-		{
-			i++;
-			swap(&arr[i], &arr[j]);
-			print_array(arr, high - low + 1);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	print_array(arr, high - low + 1);
-	return (i + 1);
+    for (j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+            print_array(arr, high - low + 1);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    print_array(arr, high - low + 1);
+    return (i + 1);
 }
 
 /**
- * quick_sort -  function that sorts an array of integers in
+ * quick_sort -  function that sorts an array of integers in 
  * ascending order using
  * the Quick sort algorithm
- * @array: input array
- * @size: size of array
+ * @arr: input array
+ * @low: index for the first element
+ * @high: index for the last element
+ *
  * Return: nothing
  */
-void quick_sort(int *array, size_t size)
+void quick_sort(int *arr, int low, int high)
 {
-	_partition(array, 0, size - 1);
+    if (low < high)
+    {
+        int index = _partition(arr, low, high);
+        quick_sort(arr, low, index - 1);
+        quick_sort(arr, index + 1, high);
+    }
 }
