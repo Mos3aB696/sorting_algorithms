256b70a7a57261ce81d892fd858a521064ebce20
Amirametwally <amira.metwally56@gmail.com>
2023-11-21 01:28:05 +0200

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,68 +1,47 @@
 #include "sort.h"
 
 /**
- * swap - function that used to swap to element
- * @xp:first element
- * @yp: sacond element
- *
- * Return: nothing
- */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-/**
- * partition - function that used tp partition the
+ * _partition - function that used tp partition the
  * array around povit
  * @arr: input array
  * @low: index for the first element
  * @high: index for the last element
  * @size: size of array
  *
- * Return: index
- */
-int partition(int arr[], int low, int high, size_t size)
-{
-	int pivot = arr[high];
-	int i = (low - 1);
-	int j;
-
-	for (j = low; j <= high - 1; j++)
-	{
-		if (arr[j] < pivot)
-		{
-			i++;
-			swap(&arr[i], &arr[j]);
-			print_array(arr, size);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	print_array(arr, size);
-	return (i + 1);
-}
-
-/**
- * quick_sort_helper -  function that sorts an array of integers in
- * ascending order using
- * the Quick sort algorithm
- * @arr: input array
- * @low: index for the first element
- * @high: index for the last element
- * @size: size of array
- *
  * Return: nothing
  */
-void quick_sort_helper(int *arr, int low, int high, size_t size)
+void partition(int arr[], int low, int high, size_t size)
 {
+	int pivot, j, i;
+	int temp;
+
 	if (low < high)
 	{
-		int index = partition(arr, low, high, size);
-
-		quick_sort_helper(arr, low, index - 1, size);
-		quick_sort_helper(arr, index + 1, high, size);
+		pivot = high;
+		j = low;
+		for (i = low; i < high; i++)
+		{
+			if (arr[i] < arr[pivot])
+			{
+				if (i != j)
+				{
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+					print_array(arr, size);
+				}
+				j++;
+			}
+		}
+		if (j != pivot && arr[j] != arr[pivot])
+		{
+			temp = arr[j];
+			arr[j] = arr[pivot];
+			arr[pivot] = temp;
+			print_array(arr, size);
+		}
+		partition(arr, low, j - 1, size);
+		partition(arr, j + 1, high, size);
 	}
 }
 
@@ -76,7 +55,5 @@ void quick_sort_helper(int *arr, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
-		return;
-	quick_sort_helper(array, 0, size - 1, size);
+	partition(array, 0, size - 1, size);
 }
