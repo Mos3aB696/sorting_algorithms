09135b66cec08ef284b97eb066dd5e712ca09e63
Amirametwally <amira.metwally56@gmail.com>
2023-11-21 00:37:26 +0200

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,56 +9,74 @@
  */
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
 
 /**
- * _partition - function that used tp partition the 
+ * _partition - function that used tp partition the
  * array around povit
  * @arr: input array
  * @low: index for the first element
  * @high: index for the last element
+ * @size: size of array
  *
- * Return: index 
+ * Return: index
  */
-int _partition(int arr[], int low, int high)
+int partition(int arr[], int low, int high, size_t size)
 {
-    int pivot = arr[high];
-    int i = (low - 1);
-    int j;
+	int pivot = arr[high];
+	int i = (low - 1);
+	int j;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-            print_array(arr, high - low + 1);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    print_array(arr, high - low + 1);
-    return (i + 1);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+			print_array(arr, size);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	print_array(arr, size);
+	return (i + 1);
 }
 
 /**
- * quick_sort -  function that sorts an array of integers in 
+ * quick_sort -  function that sorts an array of integers in
  * ascending order using
  * the Quick sort algorithm
  * @arr: input array
  * @low: index for the first element
  * @high: index for the last element
+ * @size: size of array
  *
  * Return: nothing
  */
-void quick_sort(int *arr, int low, int high)
+void quick_sort_helper(int *arr, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int index = _partition(arr, low, high);
-        quick_sort(arr, low, index - 1);
-        quick_sort(arr, index + 1, high);
-    }
+	if (low < high)
+	{
+		int index = partition(arr, low, high, size);
+		quick_sort_helper(arr, low, index - 1, size);
+		quick_sort_helper(arr, index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort -  function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @array: input array
+ * @size: size of array
+ *
+ * Return: nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size > 1)
+	{
+		quick_sort_helper(array, 0, size - 1, size);
+	}
 }
